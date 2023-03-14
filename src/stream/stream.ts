910693fb7adc56bcb0b7c9baf2eaf59ee77a92d1
ronkips <kipshillash@gmail.com>
2023-03-14 16:39:49 +0300

    updated the config file

--- src/stream/stream.ts
+++ src/stream/stream.ts
@@ -1,25 +1,20 @@
 import { BigNumber, ethers, utils } from "ethers";
-import dotenv from "dotenv";
 import { notification } from "../telegram/telegram";
 import ABI from "../../contract-abi.json";
+import { FlashLoanProviderAddress, USDCAddress, wssProvider } from "../Config/config";
 export const ListeningEvents = async () => {
-  const USDCAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-  // const WETHAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-  const FlashLoanProviderAddress = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640";
-  const provider = new ethers.providers.WebSocketProvider(process.env.WSS_URL!);
+
+  const provider = new ethers.providers.WebSocketProvider(wssProvider);
+
   const USDCContract = new ethers.Contract(USDCAddress, ABI, provider);
-  // const WETHContract = new ethers.Contract(USDCAddress, ABI, provider);
 
   const USDCfilterFrom = USDCContract.filters.Transfer(
     FlashLoanProviderAddress
   );
-  // const WETHfilterFrom = USDCContract.filters.Transfer(
-  //   FlashLoanProviderAddress
-  // );
+
 
   USDCContract.on(USDCfilterFrom, async (from, to, value, event) => {
 
-    console.log(event);
      value = (value / 1e6);
     console.log("Your data is:", to, value, event.transactionHash);
     if (Number(value) >= 10) {
