11ecabd3bbb2e1492f6dd907f461e0b5fbc7c7bc
4SAMU <samuelnzomo4@gmail.com>
2023-03-14 18:43:25 +0300

    update

--- src/telegram/messageObject.ts
+++ src/telegram/messageObject.ts
@@ -8,9 +8,8 @@ export function messageObject(
 ) {
   // Creating a message to send as a notification
   let message = `<b>FlashLoan Notifier</b>`;
-  message += `<b>${token}</b>`;
   message += `\n\n<b>TxHash</b>: https://etherscan.io/tx/${txHash}`;
-  message += `\n\n<b>Amount</b>: ${value}`;
+  message += `\n\n<b>Amount</b>: ${value} ${token}`;
   message += `\n\n<b>To</b>: https://etherscan.io/address/${to}`;
 
   return message;
