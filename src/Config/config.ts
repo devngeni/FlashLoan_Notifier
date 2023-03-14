fb4a1f8cede65c086e9861585ed1a72d69a63142
4SAMU <samuelnzomo4@gmail.com>
2023-03-14 17:15:12 +0300

    update

--- src/Config/config.ts
+++ src/Config/config.ts
@@ -7,9 +7,9 @@ const { WSS_URL } = process.env;
 
 //Tokens
 export const USDCAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-export const DaiAddress = "";
-export const WETHAddress = "";
-export const USDTAddress = "";
+export const DaiAddress = "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3";
+export const WETHAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+export const USDTAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 
 //FlashLoanProviderAddress
 export const FlashLoanProviderAddress =
